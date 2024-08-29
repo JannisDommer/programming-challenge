d2e4438c191433c0eefaa70c36df6a16e855feb7
Jannis Dommer <jannis.dommer@exxcellent.de>
2024-08-29 14:43:50 +0200

    feat(weatherChallenge): smoke test to read in csv file

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,13 @@
 package de.exxcellent.challenge;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.MissingResourceException;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -10,11 +18,26 @@ public final class App {
 
     /**
      * This is the main entry method of your program.
+     *
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
 
         // Your preparation code …
+        String weatherFilePath = "weather.csv";
+        InputStream weatherStream = App.class.getResourceAsStream(weatherFilePath);
+
+        if (weatherStream == null) {
+            throw new MissingResourceException("Unable to load resource for weather challenge", App.class.getName(), weatherFilePath);
+        }
+
+        try (CSVReader reader = new CSVReader(new InputStreamReader(weatherStream))) {
+            reader.readAll().forEach(System.out::println);
+
+        } catch (CsvException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
         String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
