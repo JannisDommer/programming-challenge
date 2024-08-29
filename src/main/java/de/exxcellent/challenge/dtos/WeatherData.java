2cf5be9f163ab577e92d166453c14f794a7b8a4d
Jannis Dommer <jannis.dommer@exxcellent.de>
2024-08-29 14:59:39 +0200

    feat(weatherChallenge): add weatherData dto

---
+++ src/main/java/de/exxcellent/challenge/dtos/WeatherData.java
@@ -0,0 +1,24 @@
+package de.exxcellent.challenge.dtos;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class WeatherData {
+    @CsvBindByName(column = "Day")
+    private int day;
+    @CsvBindByName(column = "MnT")
+    private int minTemp;
+    @CsvBindByName(column = "MxT")
+    private int maxTemp;
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMinTemp() {
+        return minTemp;
+    }
+
+    public int getMaxTemp() {
+        return maxTemp;
+    }
+}
